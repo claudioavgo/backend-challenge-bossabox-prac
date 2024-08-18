bc2158077f9f9a34e85fb67a22a842c5aa3fb571
Claudio <claudioalvesoficial@gmail.com>
2024-08-18 02:35:08 -0300

    chore: Add lazy loading for images and update npm dependency to latest stable version

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
+import { logger } from 'hono/logger'
 import toolsRoutes from "./routes/tools.routes";
 import authRoutes from "./routes/auth.routes";
 
 const app = new Hono().basePath("/api/v1/")
 
+app.use(logger())
 app.route("/", authRoutes)
 app.route("/tools", toolsRoutes)
 
